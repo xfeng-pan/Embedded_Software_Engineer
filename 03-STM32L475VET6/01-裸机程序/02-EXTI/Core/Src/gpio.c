ca754ab136015804067081544e457103f0b8fed0
xfeng-pan <panxfeng@foxmail.com>
2023-03-05 22:49:29 +0800

    外部中断（正点原子delay.c, delay.h移植）

---
+++ 03-STM32L475VET6/01-裸机程序/02-EXTI/Core/Src/gpio.c
@@ -0,0 +1,127 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+#include "delay.h"
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PE7 PE8 PE9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PD8 PD9 PD10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 1, 1);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+//	delay_ms(50);
+//	switch(GPIO_Pin)
+//	{
+//		case GPIO_PIN_10:
+//			if(HAL_GPIO_ReadPin(GPIOD,GPIO_PIN_10)==GPIO_PIN_RESET)
+//			{
+//				HAL_GPIO_TogglePin(GPIOE,GPIO_PIN_9);
+//			}
+//			break;
+//		default:
+//			break;
+//	}
+	
+	delay_ms(50);
+	if(GPIO_Pin==GPIO_PIN_8)
+	{
+		if(HAL_GPIO_ReadPin(GPIOD,GPIO_PIN_8)==GPIO_PIN_RESET)
+		{
+			HAL_GPIO_TogglePin(GPIOE,GPIO_PIN_7);
+		}
+	}
+	if(GPIO_Pin==GPIO_PIN_9)
+	{
+		if(HAL_GPIO_ReadPin(GPIOD,GPIO_PIN_9)==GPIO_PIN_RESET)
+		{
+			HAL_GPIO_TogglePin(GPIOE,GPIO_PIN_8);
+		}
+	}
+	if(GPIO_Pin==GPIO_PIN_10)
+	{
+
+		if(HAL_GPIO_ReadPin(GPIOD,GPIO_PIN_10)==GPIO_PIN_RESET)
+		{
+			HAL_GPIO_TogglePin(GPIOE,GPIO_PIN_9);
+		}
+	}
+}
+
+/* USER CODE END 2 */
