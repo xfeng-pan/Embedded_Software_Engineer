67a39e9fa61a64b796ce8ca2185fe2d937f98e51
xfeng-pan <panxfeng@foxmail.com>
2023-04-06 17:09:47 +0800

    数据结构：树形结构与二叉树，前序遍历

---
+++ 02-数据结构(C语言实现)/01-数据结构代码/05-树形结构之二叉树/树形结构之二叉树/树形结构之二叉树/Test.c
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char BTDataType;
+
+typedef struct BinaryTreeNode
+{
+	struct BinaryTreeNode* left;
+	struct BinaryTreeNode* right;
+	BTDataType data;
+}BTNode;
+
+void PreOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	printf("%c ", root->data);
+	PreOrder(root->left);
+	PreOrder(root->right);
+	//Ç°Ðò±éÀú
+}
+void InOrder(BTNode* root)
+{
+
+}
+void PostOrder(BTNode* root)
+{
+
+}
+
+int main()
+{
+	BTNode* A = (BTNode*)malloc(sizeof(BTNode));
+	A->data = 'A';
+	A->left = NULL;
+	A->right = NULL;
+
+	BTNode* B = (BTNode*)malloc(sizeof(BTNode));
+	B->data = 'B';
+	B->left = NULL;
+	B->right = NULL;
+
+	BTNode* C = (BTNode*)malloc(sizeof(BTNode));
+	C->data = 'C';
+	C->left = NULL;
+	C->right = NULL;
+
+	BTNode* D = (BTNode*)malloc(sizeof(BTNode));
+	D->data = 'D';
+	D->left = NULL;
+	D->right = NULL;
+
+	BTNode* E = (BTNode*)malloc(sizeof(BTNode));
+	E->data = 'E';
+	E->left = NULL;
+	E->right = NULL;
+
+	A->left = B;
+	A->right = C;
+	B->left = D;
+	B->right = E;
+
+	PreOrder(A);
+
+	return 0;
+}
+
+
+
+
+
+
